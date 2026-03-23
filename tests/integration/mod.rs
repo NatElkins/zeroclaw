79ab92aa06d72f29d75ed7e2dcc10a2ce99c147b
Nat Elkins <nat@nelknet.com>
2026-03-23 16:24:20 -0400

    feat(memory): add http backend for edge storage adapters

--- tests/integration/mod.rs
+++ tests/integration/mod.rs
@@ -4,6 +4,7 @@ mod backup_cron_scheduling;
 mod channel_matrix;
 mod channel_routing;
 mod hooks;
+mod hybrid_http_memory;
 #[cfg(feature = "memory-postgres")]
 mod hybrid_postgres_memory;
 mod memory_comparison;
